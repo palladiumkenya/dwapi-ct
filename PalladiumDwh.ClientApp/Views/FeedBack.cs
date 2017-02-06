51de7100f8d8db5c36e554db6febe6c9911f72fa
Koske Kimutai <koskedk@live.com>
2017-02-06 18:31:49 +0300

    initial client app

---
+++ PalladiumDwh.ClientApp/Views/FeedBack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PalladiumDwh.ClientApp.Presenters;
+
+namespace PalladiumDwh.ClientApp.Views
+{
+    public partial class FeedBack : Form, IFeedBackView
+    {
+        public FeedBack()
+        {
+            InitializeComponent();
+            Presenter = new FeedBackPresenter(this);
+            Presenter.Initialize();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            Presenter.Send();
+        }
+
+        public string Title
+        {
+            get { return Text; }
+            set { Text = value; }
+        }
+
+        public string Header
+        {
+            get { return infoControlFeedBack.Header; }
+            set { infoControlFeedBack.Header = value; }
+        }
+
+        public string HeaderDescription
+        {
+            get { return infoControlFeedBack.HeaderDescription; }
+            set { infoControlFeedBack.HeaderDescription = value; }
+        }
+
+        public IFeedBackPresenter Presenter { get; set; }
+
+
+        public int Phone
+        {
+            get { return Convert.ToInt32(textBoxPhone.Text); }
+            set { textBoxPhone.Text = value.ToString(); }
+        }
+
+        public string Email
+        {
+            get { return textBoxEmail.Text; }
+            set { textBoxEmail.Text = value; }
+        }
+
+        public string Comment
+        {
+            get { return textBoxComment.Text; }
+            set { textBoxComment.Text = value; }
+        }
+
+        public bool SendLogs
+        {
+            get { return checkBoxSendLogs.Checked; }
+            set { checkBoxSendLogs.Checked = value; }
+        }
+
+      
+    }
+}
---
+++ PalladiumDwh.ClientApp/Views/IView.cs
@@ -0,0 +1,14 @@
+﻿
+
+using PalladiumDwh.ClientApp.Presenters;
+
+namespace PalladiumDwh.ClientApp.Views
+{
+    public interface IView<T>
+    {
+        string Title { get; set; }
+        string Header { get; set; }
+        string HeaderDescription { get; set; }
+        T Presenter { get; set; }
+    }
+}
