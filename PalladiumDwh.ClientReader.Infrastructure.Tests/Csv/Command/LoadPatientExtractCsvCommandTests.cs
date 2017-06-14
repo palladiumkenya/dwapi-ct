eeb695565f4862083b9f0fac210adf15b4b77dec
Koske Kimutai <koskedk@live.com>
2017-06-14 20:40:50 +0300

    all extracts loadcsv tests fixed

--- PalladiumDwh.ClientReader.Infrastructure.Tests/Csv/Command/LoadPatientExtractCsvCommandTests.cs
+++ PalladiumDwh.ClientReader.Infrastructure.Tests/Csv/Command/LoadPatientExtractCsvCommandTests.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using NUnit.Framework;
 using PalladiumDwh.ClientReader.Core.Interfaces.Commands;
+using PalladiumDwh.ClientReader.Core.Interfaces.Repository;
+using PalladiumDwh.ClientReader.Core.Services;
 using PalladiumDwh.ClientReader.Infrastructure.Csv.Command;
 using PalladiumDwh.ClientReader.Infrastructure.Data;
 using PalladiumDwh.ClientReader.Infrastructure.Data.Repository;
+using PalladiumDwh.Shared.Custom;
 using PalladiumDwh.Shared.Model;
 
 namespace PalladiumDwh.ClientReader.Infrastructure.Tests.Csv.Command
@@ -15,53 +19,78 @@ namespace PalladiumDwh.ClientReader.Infrastructure.Tests.Csv.Command
     public class LoadPatientExtractCsvCommandTests
     {
         private DwapiRemoteContext _context;
-        private IDbConnection _sourceConnection, _clientConnection;
+        private IDbConnection _clientConnection;
         private string _commandText;
         private ILoadPatientExtractCsvCommand _extractCommand;
         private int top = 5;
-        private Progress<DProgress> _dprogress;
+        private IProgress<DProgress> _dprogress;
+        private IEMRRepository _emrRepository;
+        private string _extractsDir;
+        private string _importPath;
+        private ImportCsvService _importService;
 
 
         [SetUp]
         public void SetUp()
         {
+            _context = new DwapiRemoteContext();
+            _emrRepository = new EMRRepository(_context);
             _dprogress = new Progress<DProgress>(ReportDProgress);
-
-            _context =new DwapiRemoteContext();
-        
-            _sourceConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["EMRDatabase"].ConnectionString);
+            new SqlConnection(ConfigurationManager.ConnectionStrings["EMRDatabase"].ConnectionString);
             _clientConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DWAPIRemote"].ConnectionString);
 
-            _commandText = TestHelpers.GetCsv("_PatientExtract");
-            _extractCommand = new LoadPatientExtractCsvCommand(new EMRRepository(_context));
+            _commandText = TestHelpers.GetCsv("_PatientExtract", "Extracts");
 
-            _context.Database.ExecuteSqlCommand("DELETE FROM TempPatientExtract");          
+            _extractCommand = new LoadPatientExtractCsvCommand(_emrRepository);
+            _context.Database.ExecuteSqlCommand("DELETE FROM EventHistory");
+            _context.Database.ExecuteSqlCommand("DELETE FROM TempPatientExtract");
+
+            _extractsDir = $"{Utility.GetFolderPath(TestContext.CurrentContext.TestDirectory)}Extracts\\";
+            _importPath = $@"{TestContext.CurrentContext.TestDirectory.HasToEndsWith(@"\")}";
+
+            _importService = new ImportCsvService();
         }
 
         [Test]
         public void should_Execute_From_Csv()
         {
             Assert.That(_commandText, Does.Exist);
-            
+
+            var clearExtractsCommand = new ClearCsvExtractsCommand(_emrRepository);
+            var analyzeTempExtractsCommand = new AnalyzeCsvTempExtractsCommand(_emrRepository);
+            var result = clearExtractsCommand.ExecuteAsync(_dprogress).Result;
+
+            var files = new List<string> { _commandText };
+            var csvs = _importService.CopyCsvFilesAsync(files, _importPath).Result;
+
+            var eventHistories = analyzeTempExtractsCommand.ExecuteAsync(csvs, _dprogress).Result;
+
+
+
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            var summary= _extractCommand.ExecuteAsync(_commandText, _dprogress).Result;
+            var summary = _extractCommand.ExecuteAsync(_commandText, _dprogress).Result;
             watch.Stop();
             var records = _context.Database
                 .SqlQuery<int>("SELECT COUNT(*) as NumOfRecords FROM TempPatientExtract")
                 .Single();
-            Assert.IsTrue(records>0);
+
+            Assert.IsTrue(records == summary.Loaded);
+
+            Console.WriteLine($"Summary:{summary}");
+            Console.WriteLine($"Summary Error:{summary.ErrorStatus()}");
+
             var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine($"Loaded {records} records! in {elapsedMs}ms ({elapsedMs/1000}s)");
+            Console.WriteLine($"Loaded {records} records! in {elapsedMs}ms ({elapsedMs / 1000}s)");
             Console.WriteLine(_commandText);
         }
 
         [TearDown]
         public void TearDown()
         {
+            _context.Database.ExecuteSqlCommand("DELETE FROM EventHistory");
             _context.Database.ExecuteSqlCommand("DELETE FROM TempPatientExtract");
             _context.SaveChanges();
         }
-
         private void ReportDProgress(DProgress value)
         {
             Console.WriteLine(value);
--- PalladiumDwh.ClientReader.Infrastructure.Tests/Csv/Command/LoadPatientLaboratoryExtractCsvCommandTests.cs
+++ PalladiumDwh.ClientReader.Infrastructure.Tests/Csv/Command/LoadPatientLaboratoryExtractCsvCommandTests.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using NUnit.Framework;
 using PalladiumDwh.ClientReader.Core.Interfaces.Commands;
+using PalladiumDwh.ClientReader.Core.Interfaces.Repository;
+using PalladiumDwh.ClientReader.Core.Services;
 using PalladiumDwh.ClientReader.Infrastructure.Csv.Command;
 using PalladiumDwh.ClientReader.Infrastructure.Data;
 using PalladiumDwh.ClientReader.Infrastructure.Data.Repository;
+using PalladiumDwh.Shared.Custom;
 using PalladiumDwh.Shared.Model;
 
 namespace PalladiumDwh.ClientReader.Infrastructure.Tests.Csv.Command
@@ -15,50 +19,77 @@ namespace PalladiumDwh.ClientReader.Infrastructure.Tests.Csv.Command
     public class LoadPatientLaboratoryExtractCsvCommandTests
     {
         private DwapiRemoteContext _context;
-        private IDbConnection _sourceConnection, _clientConnection;
+        private IDbConnection _clientConnection;
         private string _commandText;
         private ILoadPatientLaboratoryExtractCsvCommand _extractCommand;
         private int top = 5;
-        private Progress<DProgress> _dprogress;
+        private IProgress<DProgress> _dprogress;
+        private IEMRRepository _emrRepository;
+        private string _extractsDir;
+        private string _importPath;
+        private ImportCsvService _importService;
 
 
         [SetUp]
         public void SetUp()
         {
-            _context=new DwapiRemoteContext();
+            _context = new DwapiRemoteContext();
+            _emrRepository = new EMRRepository(_context);
             _dprogress = new Progress<DProgress>(ReportDProgress);
-            _sourceConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["EMRDatabase"].ConnectionString);
+            new SqlConnection(ConfigurationManager.ConnectionStrings["EMRDatabase"].ConnectionString);
             _clientConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DWAPIRemote"].ConnectionString);
 
-            _commandText = TestHelpers.GetCsv("PatientLaboratoryExtract");
-            _extractCommand = new LoadPatientLaboratoryExtractCsvCommand(new EMRRepository(_context));
+            _commandText = TestHelpers.GetCsv("PatientLaboratoryExtract", "Extracts");
 
-            _context.Database.ExecuteSqlCommand("DELETE FROM TempPatientLaboratoryExtract");          
+            _extractCommand = new LoadPatientLaboratoryExtractCsvCommand(_emrRepository);
+            _context.Database.ExecuteSqlCommand("DELETE FROM EventHistory");
+            _context.Database.ExecuteSqlCommand("DELETE FROM TempPatientLaboratoryExtract");
+
+            _extractsDir = $"{Utility.GetFolderPath(TestContext.CurrentContext.TestDirectory)}Extracts\\";
+            _importPath = $@"{TestContext.CurrentContext.TestDirectory.HasToEndsWith(@"\")}";
+
+            _importService = new ImportCsvService();
         }
 
         [Test]
         public void should_Execute_From_Csv()
         {
             Assert.That(_commandText, Does.Exist);
-            
+
+            var clearExtractsCommand = new ClearCsvExtractsCommand(_emrRepository);
+            var analyzeTempExtractsCommand = new AnalyzeCsvTempExtractsCommand(_emrRepository);
+            var result = clearExtractsCommand.ExecuteAsync(_dprogress).Result;
+
+            var files = new List<string> { _commandText };
+            var csvs = _importService.CopyCsvFilesAsync(files, _importPath).Result;
+
+            var eventHistories = analyzeTempExtractsCommand.ExecuteAsync(csvs, _dprogress).Result;
+
+
+
             var watch = System.Diagnostics.Stopwatch.StartNew();
             var summary = _extractCommand.ExecuteAsync(_commandText, _dprogress).Result;
             watch.Stop();
             var records = _context.Database
                 .SqlQuery<int>("SELECT COUNT(*) as NumOfRecords FROM TempPatientLaboratoryExtract")
                 .Single();
-            Assert.IsTrue(records>0);
+
+            Assert.IsTrue(records == summary.Loaded);
+
+            Console.WriteLine($"Summary:{summary}");
+            Console.WriteLine($"Summary Error:{summary.ErrorStatus()}");
+
             var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine($"Loaded {records} records! in {elapsedMs}ms ({elapsedMs/1000}s)");
+            Console.WriteLine($"Loaded {records} records! in {elapsedMs}ms ({elapsedMs / 1000}s)");
             Console.WriteLine(_commandText);
         }
 
         [TearDown]
         public void TearDown()
         {
+            _context.Database.ExecuteSqlCommand("DELETE FROM EventHistory");
             _context.Database.ExecuteSqlCommand("DELETE FROM TempPatientLaboratoryExtract");
             _context.SaveChanges();
-
         }
         private void ReportDProgress(DProgress value)
         {
