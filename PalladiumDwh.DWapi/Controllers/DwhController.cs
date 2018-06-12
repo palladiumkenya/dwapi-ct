8ff653b61d694cb3871edbf9eb21dddae95597a6
Koske Kimutai <koskedk@live.com>
2018-06-12 09:11:53 +0300

    added dwh verification

---
+++ PalladiumDwh.DWapi/Controllers/DwhController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Http;
+using log4net;
+using PalladiumDwh.Core.Interfaces;
+using PalladiumDwh.DWapi.Models;
+using PalladiumDwh.Shared.Model;
+
+namespace PalladiumDwh.DWapi.Controllers
+{
+    public class DwhController : ApiController
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private readonly IPatientExtractRepository _patientExtractRepository;
+        private readonly IMessagingSenderService _messagingService;
+        private readonly string _gateway = typeof(Manifest).Name.ToLower();
+
+        public DwhController(IMessagingSenderService messagingService, IPatientExtractRepository patientExtractRepository)
+        {
+            _messagingService = messagingService;
+            _messagingService.Initialize(_gateway);
+            _patientExtractRepository = patientExtractRepository;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Verify([FromBody] Subscriber subscriber)
+        {
+            if (null == subscriber)
+            {
+                return BadRequest();
+            }
+
+            if (subscriber.Verify())
+                    return Ok(new
+                    {
+                        registryName= "National DataWarehouse"
+                    });
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ PalladiumDwh.DWapi/Models/Subscriber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PalladiumDwh.DWapi.Models
+{
+    public class Subscriber
+    {
+        public string SubscriberId { get; set; }
+        public string AuthToken { get; set; }
+
+        public Subscriber()
+        {
+        }
+
+        public bool Verify()
+        {
+            return SubscriberId.ToUpper() == "DWAPI" &&
+                   AuthToken.ToLower() == "1ba47c2a-6e05-11e8-adc0-fa7ae01bbebc".ToLower();
+        }
+    }
+}
