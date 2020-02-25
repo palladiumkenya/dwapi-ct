0386fe9bf717223d2cf1c77cdf007293b9813b0e
Koske Kimutai <koskedk@live.com>
2020-02-25 13:32:17 +0300

    hangfire startup

--- PalladiumDwh.DWapi/Global.asax.cs
+++ PalladiumDwh.DWapi/Global.asax.cs
@@ -23,6 +23,15 @@ namespace PalladiumDwh.DWapi
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+            JobStorage.Current = new SqlServerStorage("DWAPICentral", new SqlServerStorageOptions
+                                                                                  {
+                                                                                      CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                                                                                      SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                                                                                      QueuePollInterval = TimeSpan.Zero,
+                                                                                      UseRecommendedIsolationLevel = true,
+                                                                                      UsePageLocksOnDequeue = true,
+                                                                                      DisableGlobalLocks = true
+                                                                                  })
             _server = new BackgroundJobServer();
             Log.Debug("PalladiumDwh.DWapi started!");
         }
