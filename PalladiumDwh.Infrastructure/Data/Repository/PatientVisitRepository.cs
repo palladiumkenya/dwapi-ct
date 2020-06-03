6bb46aadc127719f368399e5fcac967a9d8a2a83
Koske Kimutai <koskedk@live.com>
2020-06-03 21:26:21 +0300

    delete logic

--- PalladiumDwh.Infrastructure/Data/Repository/PatientVisitRepository.cs
+++ PalladiumDwh.Infrastructure/Data/Repository/PatientVisitRepository.cs
@@ -146,8 +146,10 @@ namespace PalladiumDwh.Infrastructure.Data.Repository
                 // clear patient data not in register
 
                 var sql = $@"  DELETE FROM {nameof(PatientVisitExtract)} 
-                                WHERE PatientId NOT In (        
-                                    SELECT PatientId FROM {nameof(ActionRegister)}
+                                WHERE  PatientId IN @patientId AND
+
+                                PatientId NOT In (        
+                                    SELECT DISTINCT PatientId FROM {nameof(ActionRegister)}
                                     WHERE  Action=@action AND Area=@area AND PatientId IN @patientId
                                 ) 
                                 ";
