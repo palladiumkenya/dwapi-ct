b38fe8ba09dd034efe70e732bc50bea0f340fb34
Koske Kimutai <koskedk@live.com>
2022-05-05 08:51:31 +0300

    status review

--- PalladiumDwh.DWapi/Controllers/NDWHController.cs
+++ PalladiumDwh.DWapi/Controllers/NDWHController.cs
@@ -57,7 +57,7 @@ namespace PalladiumDwh.DWapi.Controllers
         {
             var   response = Request.CreateResponse(HttpStatusCode.OK, new
             {
-                name="Dwapi Central - API (CT)",status="running...",version="3.0.0.0",build="21MAR221135"
+                name="Dwapi Central - API (CT)",status="running...",version="3.0.0.1",build="05MAY220850"
             });
 
             return response;
