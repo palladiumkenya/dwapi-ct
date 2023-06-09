41d3bb8760c995e7e3bc707f12368ac9aa0ad53e
MaryKilewe <marykilewe@gmail.com>
2023-06-09 11:12:51 +0300

    added paedsdisclosure and zscore

---
+++ PalladiumDwh.Infrastructure/Migrations/202305300647466_AddedZscore.cs
@@ -0,0 +1,20 @@
+﻿namespace PalladiumDwh.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedZscore : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PatientVisitExtract", "ZScore", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.StageVisitExtract", "ZScore", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.StageVisitExtract", "ZScore");
+            DropColumn("dbo.PatientVisitExtract", "ZScore");
+        }
+    }
+}
---
+++ PalladiumDwh.Infrastructure/Migrations/202305312014266_AddedPeadsDisclosure.cs
@@ -0,0 +1,20 @@
+﻿namespace PalladiumDwh.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPeadsDisclosure : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PatientVisitExtract", "PaedsDisclosure", c => c.String(maxLength: 150));
+            AddColumn("dbo.StageVisitExtract", "PaedsDisclosure", c => c.String(maxLength: 150));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.StageVisitExtract", "PaedsDisclosure");
+            DropColumn("dbo.PatientVisitExtract", "PaedsDisclosure");
+        }
+    }
+}
