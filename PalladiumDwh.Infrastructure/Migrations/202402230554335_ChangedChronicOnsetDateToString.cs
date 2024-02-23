45f6621d02af3a76bf641b8bf97e70e27a3a495f
MaryKilewe <marykilewe@gmail.com>
2024-02-23 10:34:52 +0300

    changed chroniconsetdate datatype

---
+++ PalladiumDwh.Infrastructure/Migrations/202402230554335_ChangedChronicOnsetDateToString.cs
@@ -0,0 +1,20 @@
+﻿namespace PalladiumDwh.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedChronicOnsetDateToString : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AllergiesChronicIllnessExtract", "ChronicOnsetDate", c => c.String(maxLength: 150));
+            AlterColumn("dbo.StageAllergiesChronicIllnessExtract", "ChronicOnsetDate", c => c.String(maxLength: 150));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.StageAllergiesChronicIllnessExtract", "ChronicOnsetDate", c => c.DateTime());
+            AlterColumn("dbo.AllergiesChronicIllnessExtract", "ChronicOnsetDate", c => c.DateTime());
+        }
+    }
+}
